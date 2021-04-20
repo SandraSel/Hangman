54d346837742778bdf4a494e7a16aad4f9313fd2
Iveta Kalva <ivetar@gmail.com>
2021-04-20 15:30:07 +0300

    Added Hangman and changed some location of the functions in Hangman.scala.

--- src/main/scala/Hangman.scala
+++ src/main/scala/Hangman.scala
@@ -1,3 +1,4 @@
+
 import Utilities.setDifficulty
 
 import scala.io.StdIn.readLine
@@ -9,8 +10,6 @@ println("Welcome to the word guessing game - Hangman. You have 10 guesses.")
   val file = if (args.isEmpty) "./src/Resources/englishWords.txt" else args(0)
   var guessWords = Utilities.englishWords(file, setDifficulty())
 
-  //guessWords.foreach(println)
-
   //TODO maybe put them in a class?
   val letters: Set[Char] = Utilities.alphaSet
   val formattedPlayerInput = "\t%s [Guesses Left: %2d ] Letter: "
@@ -20,7 +19,7 @@ println("Welcome to the word guessing game - Hangman. You have 10 guesses.")
   var win = 0
   var loss = 0
 
-  while (!endGame){
+  while (!endGame) {
 
     println("Type 'Exit' to leave the game, 'New' for a new game!")
 
@@ -32,30 +31,34 @@ println("Welcome to the word guessing game - Hangman. You have 10 guesses.")
     var guessCount = 10
 
     var newGame = false
-    while (!newGame && !endGame) {
-      def checkGuesses(): Unit = {
 
-        def printResult(message: String): Unit = {
-          println("\t" + Utilities.wordJoin(splitWord) + "\n")
-          println(summary.format(message, win, loss)) //https://docs.scala-lang.org/overviews/core/string-interpolation.html
-          //https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/Formatter.html#detail
-          //https://alvinalexander.com/scala/scala-string-formatting-java-string-format-method/
-        }
-  // if the player guesses the word correctly then if versions, else looses a move
-        if (splitWord == wordUnderscoreGuess) {
-          newGame = true
-          win += 1
-          printResult("Congratulations! You won!")
-        } else {
-          guessCount match {
-            case 1 =>
-              newGame = true
-              loss += 1
-              printResult("Sorry, but try again! ;)")
-            case _ => guessCount -= 1
-          }
+    def printResult(message: String): Unit = {
+      println("\t" + Utilities.wordJoin(splitWord) + "\n")
+      println(summary.format(message, win, loss))
+    }
+
+    def checkGuesses(): Unit = {
+
+      // if the player guesses the word correctly then if versions, else looses a move
+      if (splitWord == wordUnderscoreGuess) {
+        newGame = true
+        win += 1
+        printResult("Congratulations! You won!")
+      } else {
+        guessCount match {
+          case 1 =>
+            DrawHangman.drawHangman(10)
+            newGame = true
+            loss += 1
+            printResult("Sorry, but try again! ;)")
+          case _ => guessCount -= 1
         }
       }
+    }
+
+    while (!newGame && !endGame) {
+
+      DrawHangman.drawHangman(10-guessCount)
 
       val playersInput = readLine(formattedPlayerInput.format(Utilities.wordJoin(wordUnderscoreGuess), guessCount)).toUpperCase
       playersInput match {
@@ -79,5 +82,7 @@ println("Welcome to the word guessing game - Hangman. You have 10 guesses.")
       }
     }
   }
+
   println("Thank you for playing guessing game - Hangman!")
+
 }
